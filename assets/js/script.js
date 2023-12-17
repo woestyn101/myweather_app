0cbc8583db7033e13c0dadf5a0d137bfd881cfb4
Marius Du Plooy <woestyn101@gmail.com>
2023-12-17 15:15:45 -0500

    weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,36 +11,43 @@ var humidity = document.querySelector("#humidity");
 var searchCity = document.querySelector("#searchCity");
 var searchBtn = document.querySelector("#searchBtn");
 var writeCity = document.querySelector("#writeCity");
+var currentWeatherImage = document.querySelector("#current-weather-image");
 
 //variables for day 1 forecast
 var date1 = document.querySelector("#date1")
 var temp1 = document.querySelector("#temp1");
 var wind1 = document.querySelector("#wind1");
 var humidity1 = document.querySelector("#humidity1");
+var day1Image = document.querySelector("#day1Image");
 
 //variables for day 2 forecast
 var date2 = document.querySelector("#date2")
 var temp2 = document.querySelector("#temp2");
 var wind2 = document.querySelector("#wind2");
 var humidity2 = document.querySelector("#humidity2");
+var day2Image = document.querySelector("#day2Image");
 
 //variables for day 3 forecast
 var date3 = document.querySelector("#date3")
 var temp3 = document.querySelector("#temp3");
 var wind3 = document.querySelector("#wind3");
 var humidity3 = document.querySelector("#humidity3");
+var day3Image = document.querySelector("#day3Image");
 
 //variables for day 4 forecast
 var date4 = document.querySelector("#date4")
 var temp4 = document.querySelector("#temp4");
 var wind4 = document.querySelector("#wind4");
 var humidity4 = document.querySelector("#humidity4");
+var day4Image = document.querySelector("#day4Image");
 
 //variables for day 5 forecast
 var date5 = document.querySelector("#date5")
 var temp5 = document.querySelector("#temp5");
 var wind5 = document.querySelector("#wind5");
 var humidity5 = document.querySelector("#humidity5");
+var day5Image = document.querySelector("#day5Image");
+
 
 
 
@@ -156,6 +163,79 @@ var url2 = "";
                 console.log(data.list[2].main.humidity);
                 humidity.textContent = data.list[2].main.humidity;
 
+                //console.log weather condition
+                console.log(data.list[2].weather[0].main);
+
+                imageChange(data.list[2].weather[0].main, currentWeatherImage)
+                
+              
+                 function imageChange(weatherCondition, theImage){
+
+                  if (weatherCondition === "Clouds"){
+                    console.log("clouds again");
+                    theImage.src = "https://openweathermap.org/img/wn/02d@2x.png";
+                  }
+                  else if (weatherCondition === "Clear")
+                  {
+                    console.log("clear skies");
+                    theImage.src = "https://openweathermap.org/img/wn/01d@2x.png";
+                  }
+                  else if (weatherCondition === "Rain")
+                  {
+                    console.log("It is Raining");
+                    theImage.src = "https://openweathermap.org/img/wn/10d@2x.png";
+                  }
+                  else if (weatherCondition === "Drizzle")
+                  {
+                    console.log("It is Drizzling");
+                    theImage.src = "https://openweathermap.org/img/wn/10d@2x.png";
+                  }
+                  else if (weatherCondition === "Thunderstorm")
+                  {
+                    console.log("It is thundering");
+                  theImagee.src = "https://openweathermap.org/img/wn/10d@2x.png";
+                  }
+                  else if (weatherCondition === "Snow")
+                  {
+                    console.log("It is snowing");
+                    theImage.src = "https://openweathermap.org/img/wn/10d@2x.png";
+                  }               
+                  
+                  else{
+                    console.log("Something else");
+                    theImage.src = "https://openweathermap.org/img/wn/02d@2x.png";
+                  }
+
+
+                 }
+
+               
+
+              
+          //       if (data.weather[0].main == "Clear"){
+          //         wImage.src = "https://openweathermap.org/img/wn/01d@2x.png";
+          //  }
+          //   else if (data.weather[0].main == "Clouds"){
+          //         wImage.src = "https://openweathermap.org/img/wn/02d@2x.png";
+          //  }
+          //   else if (data.weather[0].main == "Rain"){
+          //         wImage.src = "https://openweathermap.org/img/wn/10d@2x.png";
+          //  }
+          //   else if (data.weather[0].main == "Drizzle"){
+          //         wImage.src = "https://openweathermap.org/img/wn/09d@2x.png";
+          //  }
+          //   else if (data.weather[0].main == "Thunderstorm"){
+          //         wImage.src = "https://openweathermap.org/img/wn/11d@2x.png";
+          //  }
+          //   else if (data.weather[0].main == "Snow" || data.weather[1].main == "Snow"){
+          //         wImage.src = "https://openweathermap.org/img/wn/13d@2x.png";
+          //  }
+          //  else {
+          //    wImage.src = "https://openweathermap.org/img/wn/50d@2x.png";
+          //       }
+
+              
+
                 //outputting forecast day 1 weather information
 
                 console.log(convertDate(data.list[10].dt));
@@ -171,8 +251,8 @@ var url2 = "";
                 console.log(data.list[10].main.humidity);
                 humidity1.textContent = data.list[10].main.humidity;
 
-
-
+                console.log(data.list[10].weather[0].main);
+                imageChange(data.list[10].weather[0].main, day1Image)
                  
                 //outputting forecast day 2 weather information
 
@@ -189,6 +269,9 @@ var url2 = "";
                 console.log(data.list[18].main.humidity);
                 humidity2.textContent = data.list[18].main.humidity;
 
+                console.log(data.list[18].weather[0].main);
+                imageChange(data.list[18].weather[0].main, day2Image)
+                 
 
                  //outputting forecast day 3 weather information
 
@@ -205,6 +288,9 @@ var url2 = "";
                 console.log(data.list[26].main.humidity);
                 humidity3.textContent = data.list[26].main.humidity;
 
+                console.log(data.list[26].weather[0].main);
+                imageChange(data.list[26].weather[0].main, day3Image)
+
                  //outputting forecast day 4 weather information
 
                 console.log(convertDate(data.list[34].dt));
@@ -220,6 +306,9 @@ var url2 = "";
                 console.log(data.list[34].main.humidity);
                 humidity4.textContent = data.list[34].main.humidity;
 
+                console.log(data.list[34].weather[0].main);
+                imageChange(data.list[34].weather[0].main, day4Image)
+
                  //outputting forecast day 5 weather information
 
                 console.log(convertDate(data.list[39].dt));
@@ -234,6 +323,9 @@ var url2 = "";
 
                 console.log(data.list[39].main.humidity);
                 humidity5.textContent = data.list[39].main.humidity;
+
+                console.log(data.list[39].weather[0].main);
+                imageChange(data.list[39].weather[0].main, day5Image)
 
 
 
